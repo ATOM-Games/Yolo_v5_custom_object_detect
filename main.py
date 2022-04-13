b3dfa1be894d5d97f284144c400fe2d06a6956f6
ATOM-Games <80955673+ATOM-Games@users.noreply.github.com>
2022-04-13 19:01:06 +0400

    module v0.0.0.1a 'last version'

--- main.py
+++ main.py
@@ -1,30 +1,97 @@
-from YOLOv5.exp_01_v5 import Detector, setDetStatus, getDetStatus, getResImg, setResImg
-from flask import Flask, jsonify, request, make_response, render_template
+import PIL.Image
+import cv2
+from YOLOv5.exp_01_v5 import Detector, setDetStatus, getDetStatus, getResImg, setResImg, DetMy
+from flask import Flask, jsonify, request, make_response, render_template,json
 from flask_cors import CORS
+import requests
+from PIL import Image
+import numpy as np
+from base64 import  b64decode
+from cv2 import imdecode, IMREAD_COLOR
+import sys
+
+
 
 params = dict()
 for line in open("ServerConfig.cfg", "r").readlines():
     p_name, p_val = line.split("=")
     params[p_name.strip()] = p_val.strip()
-app = Flask(__name__)
+app = Flask(__name__, template_folder='web', static_folder='web')
 CORS(app)
+
+global_result = None
+
+camera = '10.0.0.195:10001'
+
+listeners = []
+
+last_res = None;
+
+def NewDetect(s1, s2):
+    setDetStatus('Wait')
+    global last_res
+    while True:
+        b64_frame = requests.post('http://'+camera+'/get_b64_frame').content
+        cv_frame = b64_to_cv(b64_frame)
+        cv_frame = cv2.resize(cv_frame, (640,640))
+        img = DetMy(cv_frame, s1, s2)
+        if img is not []:
+            #запросик
+            last_res = img
+            json_message = []
+            json_message.append({
+                "name": "weapon",
+                "res": img
+            })
+            print(json_message)
+            for listener in listeners:
+            requests.post(listener, json=json_message).content
+        else:
+            if last_res is not None:
+                last_res = None
+                json_message = []
+                json_message.append({
+                    "name": "weapon",
+                    "res": []
+                })
+                print(json_message)
+                for listener in listeners:
+                requests.post(listener, json=json_message).content
+            print("zapros")
+        if getDetStatus() == 'Wait': setDetStatus('Detect')
+        if getDetStatus() == 'OFF':
+            #setResImg(None, None)
+            break
+    print("SSSTOOOPPEEED")
+
+def b64_to_cv(b64):
+    _bytes = b64decode(b64)
+    np_arr = np.frombuffer(_bytes, dtype=np.uint8)
+    cv = imdecode(np_arr, flags=IMREAD_COLOR)
+    return cv
+
+
 
 
 @app.route('/')
 def index():
     return """<h1>I am Alive</h1>"""
 
+@app.route('/admin', methods=['GET'])
+def admin():
+    return render_template("templates/admin/index.html")
+
 @app.route('/status', methods=['POST'])
 def status():
     det = request.get_json(force=True)['detect']
-    can = request.get_json(force=True)['camera'].__str__()
+    #can = request.get_json(force=True)['camera'].__str__()
     s1 = float(request.get_json(force=True)['step_01'])
     s2 = float(request.get_json(force=True)['step_02'])
     if(getDetStatus()=='OFF') and det=='start':
-        Detector(can, s1, s2)
+        #Detector(can, s1, s2)
+        NewDetect(s1, s2)
     if(getDetStatus()=='Detect') and det=='stop':
         setDetStatus('OFF')
-
     return """<h1>I am Alive</h1>"""
 
 @app.route('/getStatus', methods=['POST'])
@@ -35,16 +102,33 @@ def getStatus():
 
 @app.route('/resultDetect', methods=['POST'])
 def resultDetect():
-    det = request.get_json(force=True)['det']
-    ND, YD = getResImg()
-    res = 'None'
-    if det == 'detect' :
-        res = YD
+    det = request.get_json(force=True).get("address")
+    if det:
+        res = requests.post(det).json()
+        return jsonify({"res_img": res["res_img"], "status": "Detect", "detectes": []})
+    else:
+        ND, dets = getResImg()
+        return jsonify({ "res_img": ND, "status" : getDetStatus(), "detectes": dets })
+
+@app.route('/www', methods=['POST'])
+def wwwww():
+    print(request.json)
+    print(request.data)
+    print(request.form)
+    return 200
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    global listeners
+    det = request.get_json(force=True).get("address")
+    det2 = request.get_json(force=True).get("method")
+    print("!!!!!"+det+" "+det2)
+    if(det in listeners):
+        print('have')
     else :
-        res = ND
-    return jsonify({ "res_img": res, "status" : getDetStatus() })
-
-
+        listeners.append(det)
+    print(listeners)
+    return jsonify({ 'm' : 'm' })
 
 
 
--- venv/YOLOv5/exp_01_v5.py
+++ venv/YOLOv5/exp_01_v5.py
@@ -14,8 +14,10 @@ from utils.general import (LOGGER, check_file, check_img_size, check_imshow, che
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
+from flask import jsonify
+from utils.augmentations import Albumentations, augment_hsv, copy_paste, letterbox, mixup, random_perspective
 
-device = select_device('сpu')
+device = select_device('cpu')
 model = DetectMultiBackend('Models/Best_weapon.pt', device=device, dnn=False, data='Models/ABC_weapon_01.yaml')
 stride, names, pt, jit, onnx, engine = model.stride, model.names, model.pt, model.jit, model.onnx, model.engine
 imgsz = check_img_size((640, 640), s=model.stride)  # check image size
@@ -32,7 +34,8 @@ elif engine and model.trt_fp16_input != half:
 
 detStatus = 'OFF'
 res_image_ND = None
-res_image_YD = None
+#res_image_YD = None
+res_detect = []
 
 def getDetStatus() :
     global detStatus
@@ -42,13 +45,15 @@ def setDetStatus(neDetStatus) :
     detStatus = neDetStatus
 def getResImg():
         global res_image_ND
-        global res_image_YD
-        return res_image_ND, res_image_YD
-def setResImg(new_img_ND, new_img_YD):
+        #global res_image_YD
+        global res_detect
+        return res_image_ND, res_detect
+def setResImg(new_img_ND, new_det):
     global res_image_ND
-    global res_image_YD
+    global res_detect
     res_image_ND = ndr_to_bs(new_img_ND)
-    res_image_YD = ndr_to_bs(new_img_YD)
+    res_detect = new_det
+
 
 def ndr_to_bs(ndr):
     if ndr is None : return None
@@ -70,26 +75,67 @@ def Detector(camera, s1, s2):
             setResImg(None, None)
             break
         im = torch.from_numpy(im).to(device)
+        print(im)
         im = im.half() if half else im.float()  # uint8 to fp16/32
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
             im = im[None]
         pred = model(im, augment=False, visualize=False)
         pred = non_max_suppression(pred, s1, s2, None, False, max_det=1000)
+        str_str=[]
         for i, det in enumerate(pred):
             p, im0, frame = path[i], im0s[i].copy(), dts.count
-            print(getDetStatus())
-            annotator = Annotator(im0, line_width=3, example=str(names))
+            print(im0)
+            #annotator = Annotator(im0, line_width=3, example=str(names))
             if len(det):
                 det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
                 for *xyxy, conf, cls in reversed(det):
                     c = int(cls)
                     label = names[c]
-                    annotator.box_label(xyxy, label, color=colors(c, True))
-            im0 = annotator.result()
+                    #annotator.box_label(xyxy, label, color=colors(c, True))
+                    str_str.append(rectjson(xyxy, label))
+            #im0 = annotator.result()
             if getDetStatus() == 'Wait' : setDetStatus('Detect')
             if getDetStatus() == 'OFF':
                 setResImg(None, None)
                 break
-            if getDetStatus() == 'Detect' and im0s[i] is not None and im0 is not None : setResImg(im0s[i], im0)
+            if getDetStatus() == 'Detect' and im0s[i] is not None and im0 is not None : setResImg(im0s[i], str_str)
     print("SSSTOOOPPEEED")
+
+def rectjson(box,n):
+    return { 'pos' : {
+        'x' : str(tensor_to_str(str(box[0]))),
+        'y' : str(tensor_to_str(str(box[1]))),
+        'w' : str(tensor_to_str(str(box[2]))),
+        'h' : str(tensor_to_str(str(box[3])))
+    } }
+
+def tensor_to_str(tensor):
+    return tensor.split("(")[1].split(".")[0]
+
+
+def DetMy(img, s1, s2):
+    im0 = img
+    img = letterbox(im0, (640, 640), stride=32)[0]
+    img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+    img = numpy.ascontiguousarray(img)
+    img = torch.from_numpy(img).to(device)
+    img = img.half() if half else img.float()  # uint8 to fp16/32
+    img /= 255  # 0 - 255 to 0.0 - 1.0
+    if len(img.shape) == 3:
+        img = img[None]
+    pred = model(img, augment=False, visualize=False)
+    pred = non_max_suppression(pred, s1, s2, None, False, max_det=1000)
+    str_str = []
+    for i, det in enumerate(pred):
+        annotator = Annotator(im0, line_width=3, example=str(names))
+        if len(det):
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+            for *xyxy, conf, cls in reversed(det):
+                c = int(cls)
+                annotator.box_label(xyxy, names[c], color=colors(c, True))
+                str_str.append(rectjson(xyxy, names[c]))
+        im0 = annotator.result()
+        cv2.imshow("2222", im0)
+        cv2.waitKey(1)
+    return str_str
